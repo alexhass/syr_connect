5e9c914bf937b71f620ec0ad88c024213787a234
alexhass <4450722+alexhass@users.noreply.github.com>
2026-02-23 00:03:49 +0100

    Fix type ignore comment for ConfigFlow class definition in config_flow.py

--- custom_components/syr_connect/config_flow.py
+++ custom_components/syr_connect/config_flow.py
@@ -119,7 +119,7 @@ class SyrConnectOptionsFlow(config_entries.OptionsFlow):
             ),
         )
 
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):  # type: ignore[call-arg]
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for SYR Connect."""
 
     VERSION = 1
