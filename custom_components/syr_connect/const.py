7f068454bfc8699f76f665b31098ca059130f249
Alexander Hass <4450722+alexhass@users.noreply.github.com>
2026-05-09 14:48:27 +0200

    Add regeneration active flag to sensor unit precision constants

--- custom_components/syr_connect/const.py
+++ custom_components/syr_connect/const.py
@@ -1278,6 +1278,7 @@ _SYR_CONNECT_SENSOR_UNIT_PRECISION = {
     "getSLF": 0,    # Self-learning phase volume (l/h)
     "getSLT": 0,    # Time in self-learning phase (seconds)
     "getSLV": 0,    # Self-learning phase volume (l)
+    "getSRE": 0,    # Regeneration active flag: values unclear
     "getSRO": 0,    # Display rotation: show as whole number by default
     "getSS1": 0,    # Salt container supply 1: show as whole number by default
     "getSS2": 0,    # Salt container supply 2: show as whole number by default
